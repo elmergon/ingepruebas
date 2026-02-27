afcf59209eb1ebbf56e272cc2740eb7643bdcfcd
elmergon <elmer.colossio@uabc.edu.mx>
2026-02-26 19:46:30 -0800

    nuevos metodos en calculadora y pruebas unitarias para ellos

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,3 +3,7 @@ from calculadora import Calculadora
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
+
+def test_subtract():
+    calc = Calculadora()
+    assert calc.subtract(10, 5) == 5
